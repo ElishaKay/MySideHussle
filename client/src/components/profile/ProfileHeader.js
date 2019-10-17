0147229147a83076d41fd5f60daed1947eee4511
Elisha Kramer <kramer1346@gmail.com>
2019-10-17 23:16:02 +0300

    removed extraneous ats for /profiles routes in each profiles header

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -21,10 +21,7 @@ class ProfileHeader extends Component {
             <div className="text-center">
               <h1 className="display-4 text-center">{profile.user.name}</h1>
               <p className="lead text-center">
-                {profile.status}{' '}
-                {isEmpty(profile.company) ? null : (
-                  <span>at {profile.company}</span>
-                )}
+                {profile.status}
               </p>
               {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
               <p>
