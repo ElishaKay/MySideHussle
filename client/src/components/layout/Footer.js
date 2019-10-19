1d9bd336e59852644f89515aced658ff029a7c65
Elisha Kramer <kramer1346@gmail.com>
2019-10-20 01:26:07 +0300

    email added

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} MySideHussle
+      <p>Got questions? Email us: Support@MySideHussle.com
+      Copyright &copy; {new Date().getFullYear()}</p>
     </footer>
   );
 };
